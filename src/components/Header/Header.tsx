4f4c9253dd4348610a7a2d75a01305460fb3265f
k-wlodarczyk <konrad.wlodarczyk99@gmail.com>
2026-05-27 21:21:42 +0200

    Remove sidebar with projects and move projects to a Select on header section - commit remaining files

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,69 @@
+import * as Select from "@radix-ui/react-select";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+import { useNavigate, useParams } from "react-router-dom";
+import { useFetchItems } from "../../hooks/useFetchItems";
 import styles from "./Header.module.css";
+import { useEffect, useState } from "react";
 
 interface HeaderProps {
   children: React.ReactNode;
 }
 
 export default function Header({ children }: HeaderProps) {
+  const navigate = useNavigate();
+  const { data: projects, isLoading } = useFetchItems("projects", "view");
+  const { projectId } = useParams();
+  const [selectedProjectId, setSelectedProjectId] = useState<
+    string | undefined
+  >(undefined);
+
+  useEffect(() => {
+    if (projectId) {
+      setSelectedProjectId(projectId);
+    }
+  }, [projectId]);
+
+  const handleProjectChange = (projectId: string) => {
+    setSelectedProjectId(projectId);
+    if (projectId) {
+      navigate(`/project/${projectId}`);
+    }
+  };
+
   return (
-    <header>
-      <h1 className={styles.header}>{children}</h1>
+    <header className={styles.header}>
+      <div className={styles.projectSection}>
+        <p>PROJECT: </p>
+        <Select.Root
+          onValueChange={handleProjectChange}
+          value={selectedProjectId || ""}
+        >
+          <Select.Trigger className={styles.selectTrigger}>
+            <Select.Value placeholder="Select project..." />
+            <Select.Icon className="SelectIcon">
+              <ChevronDownIcon />
+            </Select.Icon>
+          </Select.Trigger>
+          <Select.Portal>
+            <Select.Content position="popper" sideOffset={4}>
+              <Select.ScrollUpButton />
+              <Select.Viewport className={styles.selectViewport}>
+                {projects?.map((project: any) => (
+                  <Select.Item
+                    key={project.id}
+                    value={project.id.toString()}
+                    className={styles.selectItem}
+                  >
+                    <Select.ItemText>{project.name}</Select.ItemText>
+                  </Select.Item>
+                ))}
+              </Select.Viewport>
+            </Select.Content>
+          </Select.Portal>
+        </Select.Root>
+      </div>
+
+      <h1>{children}</h1>
     </header>
   );
 }
